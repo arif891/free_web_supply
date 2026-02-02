60c114f66a2630f8f8b902df2fa055d9010cdca6
Arif <arif.hossain.9213@gmail.com>
2026-02-02 23:00:52 +0600

    fix

--- layx/layx.js
+++ layx/layx.js
@@ -6,3 +6,4 @@ import Navbar from './components/navbar/navbar.js';
 /* Other */
 import ScrollState from './others/scroll_state/scroll_state.js';
 import ViewportTrigger from './others/viewport_trigger/viewport_trigger.js';
+import SyntaxHighlighter  from './others/syntax_highlighter/syntax_highlighter.js';
