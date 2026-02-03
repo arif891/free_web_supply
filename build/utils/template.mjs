232e6cb4481f8b0fd956dc309d7021fdf657afec
Arif <arif.hossain.9213@gmail.com>
2026-02-03 18:41:12 +0600

    .

--- build/utils/template.mjs
+++ build/utils/template.mjs
@@ -70,18 +70,7 @@ function genInventorySection(items, total = 0) {
         vanilla baseline starts here.</p>
     </div>
     <div class="x-12 sub-x gap-y-4 item__wrapper">
-      ${items.map(item => `
-      <div class="x-12 x-md-6 x-xl-4 item">
-        <img class="img" src="${item.thumbnail}" alt="">
-        <h3 class="h4 title">${item.heading}</h3>
-        <div class="tag__wrapper">
-          ${item?.tags.map(tag => `<a href="${tag?.url}">${tag?.name}</a>`).join('')}
-        </div>
-        <div class="action__wrapper">
-          <a class="btn dec_meta dec_bra dec_link action__btn" href="${item.url}">VIEW</a>
-        </div>
-      </div>
-      `).join('')}
+      ${genInventoryItems(items)}
     </div>
     <div class="x-12 info__wrapper c-md">
       <span class="d-md">STATUS: VERIFIED</span>
@@ -93,24 +82,24 @@ function genInventorySection(items, total = 0) {
     `
 }
 
+function genInventoryItems(items) {
+    return items.map(item => `
+      <div class="x-12 x-md-6 x-xl-4 item">
+        <img class="img" src="${item.thumbnail}" alt="">
+        <h3 class="h4 title">${item.heading}</h3>
+        <div class="tag__wrapper">
+          ${item?.tags.map(tag => `<a href="${tag?.url}">${tag?.name}</a>`).join('')}
+        </div>
+        <div class="action__wrapper">
+          <a class="btn dec_meta dec_bra dec_link action__btn" href="${item.url}">VIEW</a>
+        </div>
+      </div>
+      `).join('')
+}
+
 function genManifestSection(items, total = 0) {
     if (items.length > 4) items.length = 4;
     if (items.length > 0) items[0].type = 'left';
-
-    function genItem(item) {
-        return `
-        <a href="" class="${item.type === 'left' ? 'x-12 x-xl-6 item' : 'x-12 x-md-6 item'}">
-        <img class="img" src="${item.thumbnail}" alt="">
-        <div class="wrapper">
-          <h3 class="h4 title">${item.heading}</h3>
-          <div class="info__wrapper">
-            <span class="date">DATE: ${item.date}</span>
-            <span class="dec_bra dec_link">DECRYPT</span>
-          </div>
-        </div>
-      </a>
-        `
-    }
 
     return `
 <section class="manifest__section" id="manifest-section">
@@ -122,11 +111,11 @@ function genManifestSection(items, total = 0) {
       <h2 class="h4 sec__heading">DOCUMENTATION</h2>
     </div>
     <div class="x-12 x-xxl-4 left ">
-      ${items.filter(item => item.type === 'left').map(genItem).join('')}
+      ${items.filter(item => item.type === 'left').map(genManifestItems).join('')}
     </div>
     <div class="x-12 x-xxl-1 divider"></div>
     <div class="x-12 x-xxl-7 right">
-      ${items.filter(item => item.type !== 'left').map(genItem).join('')}
+      ${items.filter(item => item.type !== 'left').map(genManifestItems).join('')}
     </div>
     <div class="x-12 bottom__wrapper">
       <a class="dec_meta dec_bra dec_link" href="/pages/manifest/">VIEW_ALL</a>
@@ -134,6 +123,21 @@ function genManifestSection(items, total = 0) {
   </layout>
 </section>    
     `
+}
+
+function genManifestItems(items) {
+    return items.map(item => `
+        <a href="" class="${item.type === 'left' ? 'x-12 x-xl-6 item' : 'x-12 x-md-6 item'}">
+        <img class="img" src="${item.thumbnail}" alt="">
+        <div class="wrapper">
+          <h3 class="h4 title">${item.heading}</h3>
+          <div class="info__wrapper">
+            <span class="date">DATE: ${item.date}</span>
+            <span class="dec_bra dec_link">DECRYPT</span>
+          </div>
+        </div>
+      </a>
+        `).join();
 }
 
 function genRoot(main, left = '', right = '', other = {}) {
