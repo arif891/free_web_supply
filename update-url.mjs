6a61b10b761ce4443a5bc2f5491e5e002a779ff3
Arif <arif.hossain.9213@gmail.com>
2026-02-09 13:12:40 +0600

    .

---
+++ update-url.mjs
@@ -0,0 +1,12 @@
+import { getFilesWithExtension, readFile, writeFile } from './src/utils/fs.js';
+
+const htmlFiles = await getFilesWithExtension('/', 'html', true);
+
+htmlFiles.forEach(async file => {
+    const content = await readFile(file);
+    // Matches href="/pages/..." or href='/pages/...'
+    const updatedContent = content.replace(/href=(['"])\/pages\/([^'"]+)\1/g, (match, quote, path) => {
+        return `href=${quote}/${path}${quote}`;
+    });
+    await writeFile(file, updatedContent);
+});
