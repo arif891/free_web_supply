79b8ed12915cd1b7d56a9385655d1807907e0410
Arif <arif.hossain.9213@gmail.com>
2026-02-25 21:17:17 +0600

    .

--- assets/js/base.js
+++ assets/js/base.js
@@ -113,3 +113,18 @@ class usePreferences {
 }
 
 new usePreferences();
+
+
+const searchMenu = document.getElementById('search-menu');
+const preferenceMenu = document.getElementById('preference-menu');
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === 'k' && (event.ctrlKey || event.metaKey)) {
+        event.preventDefault();
+        searchMenu.togglePopover();
+    }
+    if (event.key === 'p' && (event.ctrlKey || event.metaKey)) {
+        event.preventDefault();
+        preferenceMenu.togglePopover();
+    }
+});
