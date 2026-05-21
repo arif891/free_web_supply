f864d9003240a9eeb39afb3c0c165c1b93b6bbc3
Arif <arif.hossain.9213@gmail.com>
2026-05-21 23:22:50 +0600

    _a 3 add with fix

--- build/templates/asset.mjs
+++ build/templates/asset.mjs
@@ -29,7 +29,7 @@ function genAssetItems(items) {
                     <use href="/assets/image/svg/icons.svg#arrow-up-right" />
                   </svg>
             </a>` : ''}
-            <a class="link" href="${item.download ?? item.thumbnail}" download title="Download">
+            <a class="link" href="${item.download ? (item.download.startsWith('/') ? `https://raw.githubusercontent.com/arif891/free_web_supply/refs/heads/main${item.download}` : item.download) : item.thumbnail}" download title="Download">
                   <svg class="icon">
                     <use href="/assets/image/svg/icons.svg#download" />
                   </svg>
---
+++ markdown/asset-3.md
@@ -0,0 +1,13 @@
+<!-- FWS / SUPPLY — New Archive Entry -->
+
+# Big Flower Pack
+
+![thumbnail](/assets/image/asset/colorful_big_flowers.avif)
+
+
+<info>
+type: asset;
+category: image;
+tags: flower, pack, colorful;
+download: /assets/public/raw/big_flower_pack.zip;
+</info>
